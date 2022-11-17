18a1fefc74d6925e48378d3328d18c9a5cb490ac
Mr-devloper <74835904+Mr-devloper@users.noreply.github.com>
2022-11-17 10:29:08 +0600

    animate toggle menu

--- app.js
+++ app.js
@@ -20,3 +20,11 @@
 
     }
 }
+// Menu bar js
+{
+    let menutoggle = document.querySelector('.toggle');
+    menutoggle.onclick = function(){
+        menutoggle.classList.toggle('active')
+        
+    }
+}
